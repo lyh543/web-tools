c6d6724d115d2435df3ea3a311dee0e1cd5d99d4
lyh543 <lyh543@outlook.com>
2026-04-30 16:59:18 +0800

    fix: Ensure Sentry is enabled based on production environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://6f9b7d7e4104464f826138049a9d6722@o4505519988998144.ingest.us.sentry.io/4505519990439936",
+  enabled: import.meta.env.PROD,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
